9d106302515fbd34901ea1eb5672d292b04e08d3
Rei Yada <ryada@c2r7p4.42perpignan.fr>
2025-01-23 14:37:02 +0100

    2025/01/23

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ryada <ryada@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 13:16:40 by ryada             #+#    #+#             */
-/*   Updated: 2025/01/23 14:03:23 by ryada            ###   ########.fr       */
+/*   Updated: 2025/01/23 14:35:48 by ryada            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -256,6 +256,129 @@ int main (int argc, char **argv)
 
 
 //GOOD ONE//
+// void ft_push_swap_turk(t_stack *stack_a, t_stack *stack_b, int argc, char **argv)
+// {
+//     //[1]Initialize the input
+//     ft_set_stack(stack_a, stack_b);
+//     ft_fill_stack(stack_a, argc, argv);
+//     if (!stack_a->top)
+//     {
+//         ft_printf("Error: stack_a is empty after initialization.\n"); //DELETE
+//         return;
+//     }
+//     ft_assign_index(stack_a);
+    
+//     if (ft_is_sorted(stack_a))
+//     {
+//         ft_printf("The input stack is already sorted.\n"); //DELETE
+//         return ;
+//     }
+//     ft_display_stack(stack_a, stack_b);
+//     ft_printf("--------------------------------------------\n");
+
+//     //[2]Push 2 nodes of stack_a to stack_b
+//     pb(stack_b, stack_a);
+//     pb(stack_b, stack_a);
+//     ft_assign_index(stack_a);
+//     ft_assign_index(stack_b);
+//     ft_display_stack(stack_a, stack_b);
+//     ft_printf("--------------------------------------------\n");
+
+//     //[3]Push the cheapest node of stack_a to stack_b
+//     while (stack_a->size > 0)
+//     {
+//         printf("1\n");
+//         ft_find_target_node(stack_a, stack_b);
+//         // if (!stack_a->cheapest->target)
+//         // {
+//         //     ft_printf("Error: Target node for cheapest is NULL.\n");
+//         //     return;
+//         // }
+//         // t_node *current_a = stack_a->top;
+//         // while (current_a)
+//         // {
+//         //     printf("Target of %d is %d\n", current_a->value, current_a->target->value);
+//         //     current_a = current_a->next;
+//         // }
+        
+//         ft_calculate_total_cost(stack_a, stack_b);
+//         ft_find_cheapest(stack_a);
+//         printf("2\n");
+//         ft_printf("\nCheapest is :[%d]%d with the cost of %d\n\n", stack_a->cheapest->index, stack_a->cheapest->value, stack_a->cheapest->cost);
+//         if (!stack_a->cheapest)
+//         {
+//             ft_printf("Error: Cheapest node is NULL.\n");
+//             return;
+//         }
+//         if (!stack_a->cheapest->target)
+//         {
+//             ft_printf("Error: Target node for cheapest is NULL.\n");
+//             return;
+//         }
+//         printf("3\n");
+//         ft_target_to_top(stack_b, stack_a->cheapest->target);
+//         ft_display_stack(stack_a, stack_b);
+//         ft_printf("--------------------------------------------\n");
+//         printf("4\n");
+//         ft_cheapest_to_top(stack_a);
+//         ft_display_stack(stack_a, stack_b);
+//         ft_printf("--------------------------------------------\n");
+//         printf("5\n");
+//         pb(stack_b, stack_a);
+//         ft_assign_index(stack_a);
+//         ft_assign_index(stack_b);
+//     }
+//     // ft_sort_3(stack_a);
+//     // ft_display_stack(stack_a, stack_b);
+//     // ft_printf("--------------------------------------------\n");
+
+//     //[4]Push back nodes of stack_b to stack_a
+//     while (stack_b->size > 0)
+//     {
+//         ft_assign_biggest_smallest(stack_b);
+//         if (stack_b->biggest->index <= stack_b->size / 2)
+//         {
+//             while (stack_b->top != stack_b->biggest)
+//                 rb(stack_b, false);
+//         }
+//         else
+//         {
+//             while (stack_b->top != stack_b->biggest)
+//                 rrb(stack_b, false);
+//         }
+//         // ft_assign_index(stack_a);
+//         // ft_assign_index(stack_b);
+//         // if (stack_a->size == 3)
+//         // {
+//         //     ft_sort_3(stack_a);
+//         //     ft_display_stack(stack_a, stack_b);
+//         //     ft_printf("--------------------------------------------\n");
+//         // }
+//         pa(stack_a, stack_b);
+//         ft_assign_index(stack_a);
+//         ft_assign_index(stack_b);
+//         ft_display_stack(stack_a, stack_b);
+//         ft_printf("--------------------------------------------\n");
+//     }
+
+//     //[5]Sort stack_a
+//     ft_assign_biggest_smallest(stack_a);
+//     if (stack_a->smallest->index <= stack_a->size / 2)
+//     {
+//         while (stack_a->top != stack_a->smallest)
+//             ra(stack_a, false);
+//     }
+//     else
+//     {
+//         while (stack_a->top != stack_a->smallest)
+//             rra(stack_a, false);
+//     }
+//     ft_display_stack(stack_a, stack_b);
+//     ft_printf("--------------------------------------------\n");
+// }
+
+
+//WITHOUT DISPLAY
 void ft_push_swap_turk(t_stack *stack_a, t_stack *stack_b, int argc, char **argv)
 {
     //[1]Initialize the input
@@ -273,38 +396,26 @@ void ft_push_swap_turk(t_stack *stack_a, t_stack *stack_b, int argc, char **argv
         ft_printf("The input stack is already sorted.\n"); //DELETE
         return ;
     }
-    ft_display_stack(stack_a, stack_b);
-    ft_printf("--------------------------------------------\n");
+    // ft_display_stack(stack_a, stack_b);
+    // ft_printf("--------------------------------------------\n");
 
     //[2]Push 2 nodes of stack_a to stack_b
     pb(stack_b, stack_a);
     pb(stack_b, stack_a);
     ft_assign_index(stack_a);
     ft_assign_index(stack_b);
-    ft_display_stack(stack_a, stack_b);
-    ft_printf("--------------------------------------------\n");
+    // ft_display_stack(stack_a, stack_b);
+    // ft_printf("--------------------------------------------\n");
 
     //[3]Push the cheapest node of stack_a to stack_b
     while (stack_a->size > 0)
     {
-        printf("1\n");
+        //printf("1\n");
         ft_find_target_node(stack_a, stack_b);
-        // if (!stack_a->cheapest->target)
-        // {
-        //     ft_printf("Error: Target node for cheapest is NULL.\n");
-        //     return;
-        // }
-        // t_node *current_a = stack_a->top;
-        // while (current_a)
-        // {
-        //     printf("Target of %d is %d\n", current_a->value, current_a->target->value);
-        //     current_a = current_a->next;
-        // }
-        
         ft_calculate_total_cost(stack_a, stack_b);
         ft_find_cheapest(stack_a);
-        printf("2\n");
-        ft_printf("\nCheapest is :[%d]%d with the cost of %d\n\n", stack_a->cheapest->index, stack_a->cheapest->value, stack_a->cheapest->cost);
+        //printf("2\n");
+        //ft_printf("\nCheapest is :[%d]%d with the cost of %d\n\n", stack_a->cheapest->index, stack_a->cheapest->value, stack_a->cheapest->cost);
         if (!stack_a->cheapest)
         {
             ft_printf("Error: Cheapest node is NULL.\n");
@@ -315,22 +426,19 @@ void ft_push_swap_turk(t_stack *stack_a, t_stack *stack_b, int argc, char **argv
             ft_printf("Error: Target node for cheapest is NULL.\n");
             return;
         }
-        printf("3\n");
+        //printf("3\n");
         ft_target_to_top(stack_b, stack_a->cheapest->target);
-        ft_display_stack(stack_a, stack_b);
-        ft_printf("--------------------------------------------\n");
-        printf("4\n");
+        // ft_display_stack(stack_a, stack_b);
+        // ft_printf("--------------------------------------------\n");
+        // printf("4\n");
         ft_cheapest_to_top(stack_a);
-        ft_display_stack(stack_a, stack_b);
-        ft_printf("--------------------------------------------\n");
-        printf("5\n");
+        // ft_display_stack(stack_a, stack_b);
+        // ft_printf("--------------------------------------------\n");
+        //printf("5\n");
         pb(stack_b, stack_a);
         ft_assign_index(stack_a);
         ft_assign_index(stack_b);
     }
-    // ft_sort_3(stack_a);
-    // ft_display_stack(stack_a, stack_b);
-    // ft_printf("--------------------------------------------\n");
 
     //[4]Push back nodes of stack_b to stack_a
     while (stack_b->size > 0)
@@ -349,11 +457,12 @@ void ft_push_swap_turk(t_stack *stack_a, t_stack *stack_b, int argc, char **argv
         pa(stack_a, stack_b);
         ft_assign_index(stack_a);
         ft_assign_index(stack_b);
-        ft_display_stack(stack_a, stack_b);
-        ft_printf("--------------------------------------------\n");
+        // ft_display_stack(stack_a, stack_b);
+        // ft_printf("--------------------------------------------\n");
     }
 
     //[5]Sort stack_a
+    ft_assign_biggest_smallest(stack_a);
     if (stack_a->smallest->index <= stack_a->size / 2)
     {
         while (stack_a->top != stack_a->smallest)
@@ -367,4 +476,3 @@ void ft_push_swap_turk(t_stack *stack_a, t_stack *stack_b, int argc, char **argv
     ft_display_stack(stack_a, stack_b);
     ft_printf("--------------------------------------------\n");
 }
-
